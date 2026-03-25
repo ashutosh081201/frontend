70b4ebe8a4aaf843fa6c54bafe0b7f789d80567d
Ashutosh Tewari <ashutoshtiwari08122001@gmail.com>
2026-03-26 01:44:45 +0530

    React-Query Code

---
+++ React-Query/react-query-r/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import PostList from './components/post-lists';
+
+function App() {
+ /*const { data, isLoading} = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+  console.log(data, isLoading);*/
+
+
+  return <div>Ashutosh
+    <PostList />
+  </div>;
+}
+
+export default App

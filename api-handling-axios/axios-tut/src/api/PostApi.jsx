0f759057ab42689f4516905683cc81bb9d6c8b27
Ashutosh Tewari <ashutoshtiwari08122001@gmail.com>
2026-03-30 01:03:03 +0530

    Handling API using Axios

---
+++ api-handling-axios/axios-tut/src/api/PostApi.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com"
+});
+
+export const getPost = () => {
+    return api.get("/posts");
+}
+
+export const deletePost = (id) => {
+    return api.delete(`/posts/${id}`);
+}
+
+export const postData = (data) => {
+    return api.post("/posts", data);
+}
+
+
+export const updateData = (id, data) => {
+    return api.put(`/posts/${id}`, data);
+}
